7b109cdf51b95977442f14b5f69bedbc8fd30cb5
itsanmol27 <anmolgarg271005@gmail.com>
2025-03-16 14:21:48 +0530

    Make home page fast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,21 @@
 import Catagories from "@/components/Catagories";
 import FeaturedProducts from "@/components/FeaturedProducts";
 import Slider from "@/components/Slider";
-import axios from "axios";
-import { allCategoriesRoute, getFeaturedProductsRoute } from "@/lib/routeProvider";
+import { Categories } from "@/lib/Categories";
+import { Products } from "@/lib/Products";
 
-export default async function Home() {
-
-  const categoriesRes = await axios.get(allCategoriesRoute)
-  const productsRes = await axios.get(getFeaturedProductsRoute);
+export default function Home() {
 
   return (
     <>
       <Slider />
       <div className="  mt-10 px-4 md:px-8 xl:px-32 2xl:px-64">
         <h1 className=" text-2xl font-semibold my-4">Featured Products</h1>
-        <FeaturedProducts products={productsRes.data.products} />
+        <FeaturedProducts products={Products} />
       </div>
       <div className="  mt-16 px-4 md:px-8 xl:px-32 2xl:px-64">
         <h1 className=" text-2xl font-semibold my-4">Categories</h1>
-        <Catagories categories={categoriesRes.data.categories} />
+        <Catagories categories={Categories} />
       </div>
     </>
   );
---
+++ src/lib/Categories.ts
@@ -0,0 +1,34 @@
+export const Categories = [
+    {
+      _id: '679fa6926369f8b4e85f333e',
+      name: 'summer',
+      displayName: 'Summer Collections',
+      image: 'https://m.media-amazon.com/images/I/61Ihgz8JotL._SY741_.jpg',
+      createdAt: '2025-02-02T17:08:34.389Z',
+      __v: 0
+    },
+    {
+      _id: '679fa6926369f8b4e85f333f',
+      name: 'winter',
+      displayName: 'Winter Collections',
+      image: 'https://m.media-amazon.com/images/I/41YMvi2psXL.jpg',
+      createdAt: '2025-02-02T17:08:34.389Z',
+      __v: 0
+    },
+    {
+      _id: '679fa6926369f8b4e85f333d',
+      name: 'electronics',
+      displayName: 'Electronics',
+      image: 'https://m.media-amazon.com/images/I/61H76AyN6pL._SL1500_.jpg',
+      createdAt: '2025-02-02T17:08:34.388Z',
+      __v: 0
+    },
+    {
+      _id: '679fa6926369f8b4e85f3340',
+      name: 'spring',
+      displayName: 'Spring Collections',
+      image: 'https://m.media-amazon.com/images/I/61ai5LJMmjL._SY741_.jpg',
+      createdAt: '2025-02-02T17:08:34.389Z',
+      __v: 0
+    }
+]
